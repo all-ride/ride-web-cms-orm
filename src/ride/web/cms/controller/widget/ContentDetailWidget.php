abf89d9b0d83ed4f9673f2222d602d77ff754cbe
Kristof Serré <kristofserre@gmail.com>
2014-09-19 14:27:00 +0200

    changed template from index to default

--- src/ride/web/cms/controller/widget/ContentDetailWidget.php
+++ src/ride/web/cms/controller/widget/ContentDetailWidget.php
@@ -242,7 +242,7 @@ class ContentDetailWidget extends AbstractWidget implements StyleWidget {
      * @return \ride\library\mvc\view\View
      */
     protected function setView(ContentProperties $contentProperties, $content) {
-        $template = $this->getTemplate(static::TEMPLATE_NAMESPACE . '/index');
+        $template = $this->getTemplate(static::TEMPLATE_NAMESPACE . '/default');
         $variables = array(
             'locale' => $this->locale,
             'widgetId' => $this->id,
