d9c8c474c3d642aeb35c99f2b0d3e8534281ab02
Kaya <kayalion@kayalion.net>
2014-12-11 12:42:30 +0100

    renamed style translation

--- src/ride/web/cms/controller/widget/ContentOverviewWidget.php
+++ src/ride/web/cms/controller/widget/ContentOverviewWidget.php
@@ -579,8 +579,8 @@ class ContentOverviewWidget extends AbstractWidget implements StyleWidget {
      */
     public function getWidgetStyleOptions() {
         return array(
-            'container' => 'label.widget.style.container',
-            'title' => 'label.widget.style.title',
+            'container' => 'label.style.container',
+            'title' => 'label.style.title',
         );
     }
 
